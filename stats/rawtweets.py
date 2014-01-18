83f61d362193f59fac91be4f597c343f7c90d158
Aadit Patel <aadit.patel@tugoticonsulting.com>
2014-01-18 09:41:04 -0800

    Changed query value from int to string

--- stats/rawtweets.py
+++ stats/rawtweets.py
@@ -4,20 +4,25 @@ c = MongoClient()
 db = c.antibullybot
 raw_tweets = db.raw_tweets
 
+bully_tweets = raw_tweets.find({"bully": "1"})
+nonbully_tweets = raw_tweets.find({"bully": "0"})
+unclassified_tweets = raw_tweets.find({"bully": {"$exists": False}})
+
+
 print "Total Tweets in DB: "
 print raw_tweets.count()
 
 print
 
 print "Total Classified Tweets: "
-print raw_tweets.find({"bully":{'exists':True}}).count()
+print raw_tweets.find({"bully":{'$exists':True}}).count()
 
 print
 
-print "Tweets Marksed as Bully: "
-print raw_tweets.find({"bully": 1}).count()
+print "Tweets Marked as Bully: "
+print bully_tweets.count()
 
 print
 
-print "Tweets Marksed as Not Bully: "
-print raw_tweets.find({"bully": 0}).count()
+print "Tweets Marked as Not Bully: "
+print nonbully_tweets.count()
