ba3283e26b97fe22c5a147942d0fc8502cfab940
Aadit Patel <aadit.patel@tugoticonsulting.com>
2014-01-18 09:25:48 -0800

    fixed print error

--- stats/rawtweets.py
+++ stats/rawtweets.py
@@ -4,10 +4,20 @@ c = MongoClient()
 db = c.antibullybot
 raw_tweets = db.raw_tweets
 
-print "Total Tweets in DB: " + raw_tweets.count()
+print "Total Tweets in DB: "
+print raw_tweets.count()
 
-print "Total Classified Tweets: " + raw_tweets.find({"bully":{'exists':True}}).count()
+print
 
-print "Tweets Marksed as Bully: " + raw_tweets.find({"bully": 1}).count()
+print "Total Classified Tweets: "
+print raw_tweets.find({"bully":{'exists':True}}).count()
 
-print "Tweets Marksed as Not Bully: " + raw_tweets.find({"bully": 0}).count()
+print
+
+print "Tweets Marksed as Bully: "
+print raw_tweets.find({"bully": 1}).count()
+
+print
+
+print "Tweets Marksed as Not Bully: "
+print raw_tweets.find({"bully": 0}).count()
