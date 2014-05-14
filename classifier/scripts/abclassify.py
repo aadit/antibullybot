a3e61226080cb177f0d682169473703640e5da47
Aadit Patel <aadit.patel@tugoticonsulting.com>
2014-05-14 13:11:28 -0700

    updated cosine similarity script

--- classifier/scripts/abclassify.py
+++ classifier/scripts/abclassify.py
@@ -1,15 +1,25 @@
 import sys
 sys.path.append('..')
 from ABClassifier.ABClassifier import ABClassifier
+import matplotlib.mlab as mlab
+import matplotlib.pyplot as plt
 
 
 ab = ABClassifier()
 
-ab.download_cursors(limit_labeled = 100)
+ab.download_cursors(limit_unlabeled = 100, limit_labeled = 100)
 ab.run_lsa(k=100)
 ab.compute_context_vectors()
 
 pws = ab.pairwise_similarity(ab.labeled_cv_list)
 
+x = np.array(pws.values())
 
+num_bins = 100
 
+fig = plt.figure()
+ax1 = fig.add_subplot(111)
+ax1.set_xlabel('Cosine Similarity')
+ax1.set_ylabel('Relative Frequency')
+ax1.set_title('Cosine Similarity in Bully Data')
+ax1.hist(x, num_bins, normed=1, facecolor='green', alpha=0.5)
