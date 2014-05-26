f130ec1dee89a7375af5c2a6525d9103caae2858
anushreeGarg <anushreegarg90@gmail.com>
2014-05-25 20:28:09 -0700

    experiment4

--- classifier/scripts/experiments/experiment_4.py
+++ classifier/scripts/experiments/experiment_4.py
@@ -1,10 +1,3 @@
-"""
-   Experiment 3: Simple Classification:
-   -Draw random unlabeled tweet, pos, and neg examples
-   -Compute Context Vectors for all 3
-   -Compute pairwise similarity pw(tweet, pos) pw(tweet, neg)
-   -Classify Tweet on higher similarity measure (or threshold?)
-"""
 
 
 
