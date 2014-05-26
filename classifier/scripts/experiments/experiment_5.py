967b0a6a8eff1b16fd4fe1244a1342cfd05380d3
Aadit Patel <aadit.patel@tugoticonsulting.com>
2014-05-26 14:36:34 -0700

    added experiment 5

--- classifier/scripts/experiments/experiment_5.py
+++ classifier/scripts/experiments/experiment_5.py
@@ -1,7 +1,7 @@
 """
-   Experiment 3: Simple Classification:
-   -Draw random unlabeled tweet, pos, and neg examples
-   -Compute Context Vectors for all 3
+   Experiment 5: Threshold Classification:
+   -Compute SVDs
+   -Compute average pos/neg 
    -Compute pairwise similarity pw(tweet, pos) pw(tweet, neg)
    -Classify Tweet on higher similarity measure (or threshold?)
 """
@@ -17,15 +17,17 @@ from sklearn.metrics.pairwise import cosine_similarity
 
 
 
-save_location = '../../experiment_data/experiment_3'
+save_location = '../../experiment_data/experiment_5'
 limit_1 = 2500
 
 k_list = [5, 10, 25, 50, 100, 150, 200, 300, 500]
+k = 150
 
+thresholds = [0.5, 0.6, 0.7, 0.75, 0.8, 0.85]
 results = {}
-for k in k_list:
+for t in thresholds:
 	results_obj = {}
-	results_obj['k'] = k
+	results_obj['t'] = t
 	results_obj['pos_list_size'] = 0
 	results_obj['neg_list_size'] = 0
 	results_obj['true_pos'] = 0
@@ -34,14 +36,16 @@ for k in k_list:
 	results_obj['true_neg_rate'] = 0
 	results_obj['accuracy'] = 0
 	results_obj['num_iterations'] = 0
-	results[k] = results_obj
+	results[int(t*100)] = results_obj
+
 
 
 
 for i in xrange(0,1):
-	for k in k_list:
+	for t in thresholds:
 
-		print "Running experiment for k = " + str(k)
+
+		print "Running experiment for t = " + str(t)
 		ab = ABClassifier()
 		ab.download_cursors(limit_unlabeled = limit_1, limit_labeled = limit_1)
 		ab.run_lsa(k=k)
@@ -52,45 +56,40 @@ for i in xrange(0,1):
 		pos_cursor = ab.labeled_collection.find({"bully":True},timeout=False).limit(limit_1)
 		neg_cursor = ab.labeled_collection.find({"bully":False},timeout=False).limit(limit_1)
 
-		unl = []
-		pos = []
-		neg = []
 
-		for u,p,n in zip(unlabeled_cursor, pos_cursor, neg_cursor):
-			
-			u_obj = {}
-			p_obj = {}
-			n_obj = {}
+		p_avg = np.zeros(k)
+		n_avg = np.zeros(k)
+		for p,n in zip(pos_cursor, neg_cursor):
 
-			u_obj["text"] = u["text"]
-			u_obj["cv"]   = ab.get_context_vector(u_obj["text"])
-			u_obj["bullying_label"] = u["bullying_label"]
-			unl.append(u_obj)
+			p_avg = p_avg + ab.get_context_vector(p["text"])
 
-			p_obj["text"] = p["text"]
-			p_obj["cv"]   = ab.get_context_vector(p_obj["text"])
-			pos.append(p_obj)
+			n_avg = n_avg + ab.get_context_vector(n["text"])
 
-			n_obj["text"] = n["text"]
-			n_obj["cv"]   = ab.get_context_vector(n_obj["text"])
-			neg.append(n_obj)
+		p_avg = p_avg/limit_1
+		n_avg = n_avg/limit_1
+
 
 		positive_set = []
 		negative_set = []
 
-		for u in unl:
+		for u in unlabeled_cursor:
 
-			p = random.choice(pos)
-			n = random.choice(neg)
+			u_obj = {}
+			u_obj["text"] = u["text"]
+			u_obj["cv"]   = ab.get_context_vector(u_obj["text"])
+			u_obj["bullying_label"] = u["bullying_label"]
 
-			pos_similarity = cosine_similarity(p["cv"], u["cv"])[0][0]
-			neg_similarity = cosine_similarity(n["cv"], u["cv"])[0][0]
+			pos_similarity = cosine_similarity(p_avg, u_obj["cv"])[0][0]
+			neg_similarity = cosine_similarity(n_avg, u_obj["cv"])[0][0]
 
-			if pos_similarity > neg_similarity:
-				positive_set.append(u)
+			#if pos_similarity > neg_similarity:
+				#positive_set.append(u_obj)
+
+			if pos_similarity >= t:
+				positive_set.append(u_obj)
 
 			else:
-				negative_set.append(u)
+				negative_set.append(u_obj)
 
 		positive_file = open(save_location + "/positive_set_" + str(k) + ".txt", 'wb')
 		negative_file = open(save_location + "/negative_set_" + str(k) + ".txt", 'wb')
@@ -113,25 +112,26 @@ for i in xrange(0,1):
 		negative_file.close()
 
 		#compute results
-		results_obj = results[k]
-		i = results[k]['num_iterations']
-		results_obj['pos_list_size'] = float(len(positive_set) + results[k]['pos_list_size'] * i)/(i+1)
-		results_obj['neg_list_size'] = float(len(negative_set) + results[k]['neg_list_size'] * i)/(i+1)
-		results_obj['true_pos'] = float(true_pos + results[k]['true_pos'] * i)/(i+1)
-		results_obj['true_neg'] = float(true_neg + results[k]['true_neg'] * i)/(i+1)
+		t = int(100*t)
+		results_obj = results[t]
+		i = results[t]['num_iterations']
+		results_obj['pos_list_size'] = float(len(positive_set) + results[t]['pos_list_size'] * i)/(i+1)
+		results_obj['neg_list_size'] = float(len(negative_set) + results[t]['neg_list_size'] * i)/(i+1)
+		results_obj['true_pos'] = float(true_pos + results[t]['true_pos'] * i)/(i+1)
+		results_obj['true_neg'] = float(true_neg + results[t]['true_neg'] * i)/(i+1)
 		results_obj['true_pos_rate'] = float(results_obj['true_pos'])/results_obj['pos_list_size']
 		results_obj['true_neg_rate'] = float(results_obj['true_neg'])/results_obj['neg_list_size']
 		results_obj['accuracy'] = float(results_obj['true_pos'] + results_obj['true_neg'])/(results_obj['pos_list_size'] + results_obj['neg_list_size'])
 		results_obj['num_iterations'] = i + 1
-		results[k] = results_obj
+		results[t] = results_obj
 
 		"Done. Files saved at " + save_location
 
 
-for k in k_list:
-	r = results[k]
+for t in thresholds:
+	r = results[int(t*100)]
 	print " " 
-	print "k: %i" % (r["k"])
+	print "t: %i" % (r["t"])
 	print "num positive: %i " % (r["pos_list_size"])
 	print "num negative: %i" % (r["neg_list_size"])
 	print "true pos rate: %f" % (r['true_pos_rate'])
